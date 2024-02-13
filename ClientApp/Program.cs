104048d7172395720fa1ce0f6cd2080f65939a81
Stefano Vuerich <vuerich@irsweb.it>
2024-02-13 16:18:05 +0100

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using MyClassLibrary;
+
+Console.WriteLine("Hello, World!");
+
+//MyService service = new MyService();
+//service.SendMail();
+
+IMailService serviceGmail = new MailServiceGmail();
+serviceGmail.SendMail("from@gmail.com", "to@gmail.com");
---
+++ MyClassLibrary/MailServiceGmail.cs
@@ -0,0 +1,16 @@
+﻿namespace MyClassLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MailServiceGmail : IMailService
+    {
+        public void SendMail(string from, string to)
+        {
+            Console.WriteLine($"Sending mail to {to} from {from} with gmail");
+        }
+    }
+}
